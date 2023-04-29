9a77394633a502b8aac989e69619cf66f371c5d0
charlietren <71682352+charlietren@users.noreply.github.com>
2023-04-29 17:26:44 +1000

    navigation to pg 2

--- Correlation.py
+++ Correlation.py
@@ -1,6 +1,33 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from streamlit.components.v1 import html
+
+#cheeky js to navigate to other pages src: https://github.com/streamlit/streamlit/issues/4832
+def nav_page(Sensitivity_Slider, timeout_secs=3):
+    nav_script = """
+        <script type="text/javascript">
+            function attempt_nav_page(page_name, start_time, timeout_secs) {
+                var links = window.parent.document.getElementsByTagName("a");
+                for (var i = 0; i < links.length; i++) {
+                    if (links[i].href.toLowerCase().endsWith("/" + page_name.toLowerCase())) {
+                        links[i].click();
+                        return;
+                    }
+                }
+                var elasped = new Date() - start_time;
+                if (elasped < timeout_secs * 1000) {
+                    setTimeout(attempt_nav_page, 100, page_name, start_time, timeout_secs);
+                } else {
+                    alert("Unable to navigate to page '" + page_name + "' after " + timeout_secs + " second(s).");
+                }
+            }
+            window.addEventListener("load", function() {
+                attempt_nav_page("%s", new Date(), %d);
+            });
+        </script>
+    """ % (Sensitivity_Slider, timeout_secs)
+    html(nav_script)
 
 st.title('Select Element and Correlation')
 
@@ -83,5 +110,11 @@ if file is not None:
 
             #button to confirm sensitivity selection
             next2 = st.form_submit_button("Visualise")
+
+#button to go to page 2
+with st.form("Go to Page 2 Button"):
+    sensitivity_button = st.form_submit_button("Go to Page 2")
+if sensitivity_button:
+    nav_page("Sensitivity_Slider")
 
     
